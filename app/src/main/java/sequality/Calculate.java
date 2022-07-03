6a6787ede5c45668f9f135fc797cd00d1df1a98a
unknown <m1m22a27@st.oit.ac.jp>
2022-07-03 17:52:21 +0900

    add kadai

--- app/src/main/java/sequality/Calculate.java
+++ app/src/main/java/sequality/Calculate.java
@@ -4,4 +4,18 @@ public class Calculate {
   public int sum(int x, int y) {
     return x + y;
   }
+
+  public double ave(int sum, int n) {
+    return sum / n;
+  }
+
+  public void odd_eve(int a, int b,int add,int eve) {
+    for (int i = a; i <= b; i++) {
+      if (i % 2 == 0) {
+        add = add + i;
+      } else {
+        eve = eve + i;
+      }
+    }
+  }
 }
